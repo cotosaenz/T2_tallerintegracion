c060e27adce632b926cec0a38c3be60848c8fbfa
jose saenz <jisaenz@uc.cl>
2021-05-02 20:05:08 -0400

    deploy a heroku

--- myapi/settings.py
+++ myapi/settings.py
@@ -129,10 +129,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-import dj_database_url
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
-
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
